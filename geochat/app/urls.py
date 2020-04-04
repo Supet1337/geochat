0be2b6898bfea1e46ff0b3458473c136aa278377
Maksim Rubinov <maksrubinov@mail.ru>
2020-04-04 17:22:49 +0300

    Ограничения для не вошедшего пользователя

--- geochat/app/urls.py
+++ geochat/app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('ajax-maps-draw/<int:number>',ajax_maps_draw),
     path('ajax-circle-draw',ajax_circle_draw),
     path('ajax-circle-draw-joined',ajax_circle_draw_joined),
-    path('ajax-load-messages/<int:number>',ajax_update),
+    path('ajax-load-messages/<int:number>',ajax_load_messages),
     path('profile/<int:number>',profile),
 ]
