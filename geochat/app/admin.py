292f359d2a53f4dbe8b0247f080e99c6b852d736
Рубинов Максим <maksrubinov@mail.ru>
2020-04-05 11:20:48 +0000

    Update admin.py

--- geochat/app/admin.py
+++ geochat/app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Wallet
+from .models import Wallet, Room, Message, JoinRoom
 
 admin.site.register(Wallet)
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(JoinRoom)
 # Register your models here.
