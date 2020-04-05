af7399b699476002c1d7d75c8edbe31ab7ed11a4
Рубинов Максим <maksrubinov@mail.ru>
2020-04-05 12:36:28 +0000

    Update urls.py

--- geochat/geochat/urls.py
+++ geochat/geochat/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from geochat import settings
+
+url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
 
 
 urlpatterns = [
