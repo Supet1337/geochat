a3891ec557a767dad0dfb6a75ff0fb71e3099942
Пётр Воронцов <petr.vorontsov2003@gmail.com>
2020-05-19 17:21:25 +0300

    ci

--- geochat/app/views.py
+++ geochat/app/views.py
@@ -51,7 +51,7 @@ def room_sort(rooms, roomsj):
         flag = False
 
 
-def find_image(context, user_add, name):
+def find_image(context, user_add, nameeee):
     """
     Функция поиска аватара.
 
