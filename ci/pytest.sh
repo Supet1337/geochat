c2a75f3602c2cf92da60c1ee9d78db104eb13b19
Воронцов Петр <petr.vorontsov2003@gmail.com>
2020-06-02 19:13:57 +0000

    Update pytest.sh

--- ci/pytest.sh
+++ ci/pytest.sh
@@ -4,11 +4,11 @@ echo "Накатываем миграции"
 python3 geochat/manage.py makemigrations
 python3 geochat/manage.py migrate
 cd geochat/
-
+mkdir public
 echo "Запускаем тесты"
 pytest --cov -v -x | tee pytest.txt
 coverage html
-mkdir public
+
 coverage=$(sed -n 's/^TOTAL *[0-9]* *[0-9]* *\([0-9]*\)%/\1/p' pytest.txt)
 echo "Покрытие кода: $coverage%"
 
